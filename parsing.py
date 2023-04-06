6c20e17c0168950b83662a591e261bb8a03fef07
David Stekol <dstekol@gmail.com>
2023-04-06 15:48:12 -0400

    sped up search by only reading in rules once, working on getting pointers for backtracing in now

--- parsing.py
+++ parsing.py
@@ -2,22 +2,20 @@ import numpy as np
 import nltk
 
 
-def findNonTerm(aList, bList, productions):
+def findNonTerm(aList, bList, aMat, bMat, termMat):
     aTerms = aList.split(',')
     bTerms = bList.split(',')
     store = ""
     for aTerm in aTerms:
         for bTerm in bTerms:
-            for production in productions:
-                prodTokens = nltk.word_tokenize(production)
-                if len(prodTokens) == 5:
-                    #print(prodTokens)
-                    if aTerm == prodTokens[3] and bTerm == prodTokens[4]:  #issue is it isn't checking the order
-                        if store == "":
-                            store = prodTokens[0]
-                        else:
-
-                            store = store + ","+ prodTokens[0]
+            indices = [i for i, x in enumerate(aMat) if x == aTerm]
+            #print(indices)
+            for index in indices:
+                if bMat[index] == bTerm:
+                    if store == "":
+                        store = termMat[index]
+                    else:
+                        store = store + ","+ termMat[index]
 
     return store
 
@@ -27,6 +25,15 @@ print("Hello world")
 
 cnfFile = open("./sampleGrammar.cnf",'r')
 productions =  cnfFile.readlines()
+nonTermList = []
+aList = []
+bList = []
+for production in productions:
+    prodTokens = nltk.word_tokenize(production)
+    if len(prodTokens) == 5:
+        nonTermList.append(prodTokens[0])
+        aList.append(prodTokens[3])
+        bList.append(prodTokens[4])
 while True:
     #test case is "book the flight through houston"
     
@@ -35,39 +42,49 @@ while True:
         quit()
     tokens = nltk.word_tokenize(sentence)
     offset = 0
-    #parseMat = [ [""]*len(tokens) for i in range(len(tokens))]
-    parseMat =  np.array([["" for i in range(len(tokens))] for i in range(len(tokens))], dtype = object)
+    parseMat = [ [""]*len(tokens) for i in range(len(tokens))]
+    #parseMat =  np.array([["" for i in range(len(tokens))] for i in range(len(tokens))], dtype = object)
     #print(parseMat)
     for token in tokens:
         for production in productions:
             prodTokens = nltk.word_tokenize(production)
             if token in prodTokens:
-                if parseMat[offset,offset] == "":
-                    parseMat[offset,offset] = prodTokens[0]
+                if parseMat[offset][offset] == "":
+                    parseMat[offset][offset] = prodTokens[0]
                 else:
-                    parseMat[offset,offset] = parseMat[offset,offset] + "," + prodTokens[0]
+                    parseMat[offset][offset] = parseMat[offset][offset] + "," + prodTokens[0]
                 #print(prodTokens[0])
         offset = offset + 1
         #printing goes from lowest row to hightest row
-    #print(parseMat)
+    print(parseMat)
     productSize = 2
+    callNum = 0
     #as product size increases, start of diagonal shifts one to the right, and you have one less check
     for productSize in range(1, len(tokens)):
         for colCount in range (productSize, len(tokens)):
-            print(colCount)
+            #print(colCount)
             row = (colCount-productSize)
             #aMat and bMat are each productSize steps out in each direction from their given square
-            aMat = parseMat[row,colCount-productSize:colCount]
+            aMat = parseMat[row][colCount-productSize:colCount]
             tempPos = row+productSize  
-            bMat = parseMat[row+1:tempPos+1,colCount]
+            bMat = []
+            for tempRow in range(row+1,tempPos+1):
+                bMat.append(parseMat[tempRow][colCount])
+            #bMat = parseMat[row+1:tempPos+1][colCount]
             for i in range(len(aMat)):
-                
-                output = findNonTerm(aMat[i], bMat[i], productions)
+                if aMat[i] == "" or bMat[i] == "":
+                    output = ""
+                else:
+                    #print("before call")
+                    #print(callNum)
+                    output = findNonTerm(aMat[i], bMat[i], aList, bList, nonTermList)
+                    #print("after call")
+                    callNum = callNum + 1
                 if output != "":
-                    if parseMat[row, colCount] == "":
-                        parseMat[row,colCount] = output
+                    if parseMat[row][colCount] == "":
+                        parseMat[row][colCount] = output
                     else:
-                        parseMat[row,colCount] = parseMat[row,colCount] + "," + output 
+                        parseMat[row][colCount] = parseMat[row][colCount] + "," + output 
             #each value at a given index in aMat is followed by the value in the given index at bMat
             #parseMat[row][colCount] = parseMat[row][colCount] + "visited, "
 
@@ -83,9 +100,13 @@ while True:
        # while(localOffset<= len(tokens)-1-productSize):
         #    local
     
-    #print(parseMat)
-    final = len(parseMat[0,len(tokens)-1].split(','))
-    print(final)
+    print(parseMat)
+
+    final = len(parseMat[0][len(tokens)-1].split(','))
+    if final == 0:
+        print("No valid parses")
+    else:
+        print(str(final) + " valid parses")
 
 
 
